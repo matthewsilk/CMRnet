d338ac794565704177942ec47022a3f077f77e1d
Matthew Silk <matthewsilk@outlook.com>
2020-06-18 14:17:31 +0100

    updated plotting function documentation

--- R/cmrMovPlot.R
+++ R/cmrMovPlot.R
@@ -8,11 +8,11 @@
 #'@param locs A matrix or dataframe giving x and y coordinates of all nodes in the full network. Can be used to provide user-defined layouts to the graph - helpful for plotting locations in geographic space
 #'@param dynamic (TRUE/FALSE). If true this function will plot networks for the different network windows sequentially in the same plot window with a delay. If false then this function will plot the networks in a single, multipanel figure
 #'@param rows Sets the number of rows in the multipanel figure is dynamic=FALSE
+#'@param \dots Extra arguments from \code{\link[igraph]{plot.igraph}} if desired
 #'@details This function plots movement networks produced by CMRnet. As well as the arguments detailed above, additional arguments from plot.igraph can be used to produce plots desired by the user
-#'#'@return A series of network plots showing the movement network from each network window
-#' #'@examples
+#'@return A series of network plots showing the movement network from each network window
+#'@examples
 #'\dontrun{
-#'# example without multiple layers ####
 #'
 #'# load in data
 #'data(cmrData)
@@ -36,6 +36,8 @@
 #'# convert to networks
 #'networks<-cmr_igraph(movenetdat,type="movement")
 #'cmrSocPlot(nets=networks,fixed_locs=TRUE,dynamic=FALSE,rows=2)
+#'}
+#'@export
 
 cmrMovPlot<-function(nets,fixed_locs=c(TRUE,FALSE),locs=NULL,dynamic=c(TRUE,FALSE),rows=3, ...){
   L<-length(nets[[1]])
@@ -50,7 +52,7 @@ cmrMovPlot<-function(nets,fixed_locs=c(TRUE,FALSE),locs=NULL,dynamic=c(TRUE,FALS
 
   if(dynamic==FALSE){
     nws<-length(nets[[1]])
-    par(mar=c(1,1,1,1),mfrow=c(rows,ceiling(nws/rows)))
+    graphics::par(mar=c(1,1,1,1),mfrow=c(rows,ceiling(nws/rows)))
     for(i in 1:nws){
       if(fixed_locs==FALSE&length(locs)==0){
         lo2<-igraph::layout_with_fr(nets[[1]][[i]])
@@ -61,7 +63,7 @@ cmrMovPlot<-function(nets,fixed_locs=c(TRUE,FALSE),locs=NULL,dynamic=c(TRUE,FALS
   }
   if(dynamic==TRUE){
     nws<-length(nets[[1]])
-    par(mar=c(1,1,1,1),mfrow=c(1,1))
+    graphics::par(mar=c(1,1,1,1),mfrow=c(1,1))
     for(i in 1:nws){
       if(fixed_locs==FALSE&length(locs)==0){
         lo2<-igraph::layout_with_fr(nets[[1]][[i]])
