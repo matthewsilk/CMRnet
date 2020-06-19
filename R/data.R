f27b7247d7e1583ec6afb96bbffbdcce35a531d9
Matthew Silk <matthewsilk@outlook.com>
2020-06-19 11:33:56 +0100

    Added cmrData3
    An example dataset with greater temporal resolution

--- R/data.R
+++ R/data.R
@@ -1,7 +1,7 @@
 #' cmrData
 #' cmrData
 #'
-#'cmrData can be used to test social and movement ntwork functions of CMRnet. It contains five columns: the ID column provides the identity of the individual captured, loc is the named capture location, x and y provide the geographic location of a capture event, and date provide the capture time.
+#'cmrData can be used to test social and movement ntwork functions of CMRnet. It contains five columns: the ID column provides the identity of the individual captured, loc is the named capture location, x and y provide the geographic location of a capture event, and date provide the capture date.
 #'
 #'
 #' @format A data frame with five variables: \code{id}, \code{loc},
@@ -10,8 +10,17 @@
 
 #' cmrData2
 #'
-#'cmrData2 to test the multiplex movement network function.It contains six columns: the ID column provides the identity of the individual captured, loc is the named capture location, x and y provide the geographic location of a capture event, date provides the capture time and layer provide information used to define the layers of the multiplex network.
+#'cmrData2 to test the multiplex movement network function.It contains six columns: the ID column provides the identity of the individual captured, loc is the named capture location, x and y provide the geographic location of a capture event, date provides the capture date and layer provide information used to define the layers of the multiplex network.
 #'
 #' @format A data frame with six variables: \code{id}, \code{loc},
 #'   \code{x}, \code{y}, \code{date} and \code{layer}. \code{cmrData2} is an example of how dataframes should be created to feed into CMRnet when there are multiple layered movement networks (i.e. a separate network for males and females).
 "cmrData2"
+
+#' cmrData3
+#'
+#'cmrData3 to test short-term social and movement network functions of CMRnet.It contains five columns: the ID column provides the identity of the individual captured, loc is the named capture location, x and y provide the geographic location of a capture event, and date provide the capture date and time.
+#'
+#' @format A data frame with five variables: \code{id}, \code{loc},
+#'   \code{x}, \code{y} and \code{date}. \code{cmrData3} is an example of how dataframes should be created to feed into CMRnet when capture times are relevant as well as capture dates.
+"cmrData3"
+
